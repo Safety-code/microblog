de19f91ea727b79e9123266e3c70076254906673
Safety-code <jomens136@gmail.com>
2025-06-25 09:10:37 +0000

    Added new templates files

--- app/routes.py
+++ app/routes.py
@@ -34,3 +34,7 @@ def login():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
