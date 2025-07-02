74e2c605a43736a7b6158255d48685900c66330e
Safety-code <jomens136@gmail.com>
2025-07-02 15:04:54 +0000

    Added backend auth files

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,45 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.forms import LoginForm
 from flask import flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app.models import User
+from app import db
+from urllib.parse import urlsplit
 
+#VIEW FUNCTIONS
 #main/home page
 @app.route('/')
 @app.route('/index')
+@login_required  #protects login view function against anonymous users
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
-#login page
+#login page and user authentication
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)   
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc !='':
+            next_page = url_for('index')
+        return redirect(next_page)
+#logout page
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+  
 
 #About page
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
