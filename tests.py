4b6ea90a6445a583c89a572a8bdadcc46944b74a
Safety-code <jomens136@gmail.com>
2025-07-17 00:52:45 +0000

    followers association table and many-many relationship

---
+++ tests.py
@@ -0,0 +1,8 @@
+import os
+os.environ['DATABASE_URI'] = 'sqlite://'
+
+from datetime import datetime, timezone, timedelta
+import unittest
+from app import app, db
+from app.models import User, Post
+
